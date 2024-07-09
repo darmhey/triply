813feff60ac100e8890e607dc4ff666b3088b89a
Damilola <johnasco19@gmail.com>
2024-07-09 22:52:43 +0100

    connecting mongoDB

--- app.js
+++ app.js
@@ -7,16 +7,10 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 app.use(express.json());
 
-//MIDDLEWARE
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-// app.use((req, res, next) => {
-//   console.log("Hello from the middleware");
-//   next();
-// });
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
--- server.js
+++ server.js
@@ -1,11 +1,64 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({
   path: "./config.env",
 });
+
 const app = require("./app");
 
-console.log(process.env);
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://darmhey:qwerdffgr1234jkjj@cluster0.ddguiqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log("DB connection successful");
+//   })
+//   .catch((err) => {
+//     console.error(
+//       "Failed to connect to MongoDB............................................",
+//       err
+//     );
+//   });
+
+//console.log(process.env);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

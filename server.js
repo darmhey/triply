bf2654ab5379c273dccac41e6fb021dfe7ce7732
Damilola <johnasco19@gmail.com>
2024-07-10 08:36:41 +0100

    create documents in mongoose

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
     //useFindAndModify: false,
   })
   .then(() => {
@@ -28,7 +28,39 @@ mongoose
     );
   });
 
-//console.log(process.env);
+//mongoose schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+//mongoose model
+const Tour = mongoose.model("Tour", tourSchema);
+
+//creating a test document
+const testTour = new Tour({
+  name: "The Mountain hiker",
+  rating: 4.8,
+  price: 344,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log(`ERROR 👹: ${err}`));
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);

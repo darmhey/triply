848569855437a0f04a6d28d4b404309888fd6fdb
Damilola <johnasco19@gmail.com>
2024-07-11 16:21:07 +0100

    complex queries for tours api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -26,7 +26,59 @@ const Tour = require("./../models/tourModel");
 // };
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY BE MUTATING QUERY OBJ
+    //1a - filtering
+    const queryObj = { ...req.query };
+    excludeFields = ["page", "sort", "limit", "fields"];
+    excludeFields.forEach((element) => {
+      delete queryObj[element];
+    });
+
+    //1b - advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2 - sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    //3 - limiting fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    //4 - Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+    console.log(req.query, queryObj);
+
+    // const tours = await Tour.find()
+    //   .where("duration")
+    //   .equals(5)
+    //   .where("difficulty")
+    //   .equals("easy");
+
     res.status(200).json({
       status: "success",
       results: tours.length,
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trim: true,
   },
   rating: {
     type: Number,
@@ -17,25 +18,49 @@ const tourSchema = new mongoose.Schema({
   },
   duration: {
     type: Number,
-    required: [true, "A tour must have a duration"],
+    required: [true, "A tour must have duration"],
   },
   maxGroupSize: {
     type: Number,
-    required: [true, "A tour must have a group size"],
+    required: [true, "A tour must have group size"],
   },
   difficulty: {
     type: String,
-    required: [true, "A tour must have a difficulty"],
-    enum: {
-      values: ["easy", "medium", "difficult"],
-      message: "Difficulty is either: easy, medium, difficult",
-    },
+    required: [true, "A tour must have difficulty level"],
+    // enum: {
+    //   values: ["easy", "medium", "difficult"],
+    //   message: "Difficulty is either: easy, medium, difficult",
+    // },
   },
   ratingsAverage: {
     type: Number,
     default: 4.5,
-    min: [1, "Rating must be above 1.0"],
-    max: [5, "Rating must be below 5.0"],
+    // min: [1, "Rating must be above 1.0"],
+    // max: [5, "Rating must be below 5.0"],
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have description"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
   },
 });
 

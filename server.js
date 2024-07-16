6914c25489d7a8bb69b01220a5a7ca1287827368
Damilola <johnasco19@gmail.com>
2024-07-16 09:54:28 +0100

    create user and auth conrtoller

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: "./config.env",
 });
@@ -20,15 +26,17 @@ mongoose
   })
   .then(() => {
     console.log("DB connection successful");
-  })
-  .catch((err) => {
-    console.error(
-      "Failed to connect to MongoDB............................................",
-      err
-    );
   });
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });

994e77979f150e49ca58a7793b185b46563f9b86
Damilola <johnasco19@gmail.com>
2024-07-17 17:10:03 +0100

    jws signin update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -10,14 +17,33 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: "success",
     token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //check if email and password is correct
+  if (!email || !password) {
+    return next(new AppError("Please provide Email and Password", 400));
+  }
+  //check if user exists and password is correct
+  const user = await User.findOne({ email }).select("password");
+  const passwordMatch = await user.passwordMatch(password, user.password);
+
+  if (!user || !passwordMatch) {
+    return next(new AppError("Email or password incorrect", 401));
+  }
+  //if everything is okay, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: "success",
+    token,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please provide password"],
     minLength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -42,6 +43,13 @@ userSchema.pre("save", async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.passwordMatch = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 

197a9bcbcc55d57568d40957576644031647d5a5
Damilola <johnasco19@gmail.com>
2024-07-26 08:50:53 +0100

    added booking routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const globalError = require("./controllers/errorController");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+//const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRouter);
+//app.use("/api/v1/reviews", reviewRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`), 404);
